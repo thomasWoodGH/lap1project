818677055e8f5072cebd257056dec140a3f9a1f0
saranya <nsara.uma@gmail.com>
2024-01-22 14:51:48 +0000

    Added few lines in index.html

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require("./logger")
+const port = 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the questions API")
+})
+
+app.listen(port, () => {
+    console.log(`API listening on port ${port}.`);
+}) 
