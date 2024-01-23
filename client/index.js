fff6708884cb6084b745521c6dfcfe45ed8916e8
malak <malookaabdalla@gmail.com>
2024-01-23 13:25:25 +0000

    add questions answers to the end of the page

--- client/index.js
+++ client/index.js
@@ -4,19 +4,39 @@ const generateRandomId = (indexArr) => {
   let index = Math.floor(Math.random() * indexArr.length);
   let id = indexArr[index];
   indexArr.splice(index, 1);
-  return id;
+  questionData(id);
 };
-console.log(generateRandomId(indexArr));
 
 const questionData = async (id) => {
   try {
     const response = await fetch(`http://localhost:3000/questions/${id}`);
+    const data = await response.json();
+
+    Qs.textContent = data.question;
+    a.textContent = data.choices[0];
+    b.textContent = data.choices[1];
+    c.textContent = data.choices[2];
+    d.textContent = data.choices[3];
+    
   } catch (e) {
     console.log(e);
   }
 };
 
-console.log(questionData())
+let Qs = document.querySelector('#quizQuestion');
+let a = document.querySelector('#answerA');
+let b = document.querySelector('#answerB');
+let c = document.querySelector('#answerC');
+let d = document.querySelector('#answerD');
+
+for (let i = 1; i <= 36; i++) {
+    const button = document.getElementById(`button ${i}`);
+    if (button) {
+      button.addEventListener("click", () => {
+        generateRandomId(indexArr);
+      });
+    }
+  }
 //pop
 
 document.addEventListener("DOMContentLoaded", () => {
